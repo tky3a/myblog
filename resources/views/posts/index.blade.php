08e5e2ca727508699df9cdb7944228a3e8d485de
tky3a <takayamac@icloud.com>
2018-05-18 14:22:22 +0900

    add a delete view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,9 +18,16 @@
           <li>
             <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
             <a href="{{ action('PostsController@edit', $post) }}" class="edit">[Edit]</a>
+            <a href="#" class="del" data-id="{{ $post->id }}">[X]</a>
+            <form class="" action="{{ url('/posts/', $post->id ) }}" method="post"
+              id="form_{{$post->id }}">
+              {{ csrf_field() }}
+              {{ method_field('delete') }}
+            </form>
           </li>
           @empty
           <li>No posts yet</li>
           @endforelse
         </ul>
+        <script src="/js/main.js"></script>
       @endsection
